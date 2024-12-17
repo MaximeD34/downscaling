8152b1a8cb066a694eb8be4403413bd64a8e01eb
mathilde.ducasse <mathilde.ducasse@etu.umontpellier.fr>
2024-12-17 15:12:39 +0100

    C'ets toi tu degoute maxime.

--- dashboard/app.py
+++ dashboard/app.py
@@ -19,16 +19,25 @@ if name:
 dataHR_dict = torch.load("../serialized_data/dataHR.pt")
 dataHR = dataHR_dict["data"].numpy()
 
-to_plot = dataHR[3, 329, 0]
+# "video" avec slider 
+num_frames = dataHR.shape[1]
+frame = st.slider("Temps", min_value=1, max_value=num_frames , value=1)
+to_plot = dataHR[3, frame, 0, :, : ]
 
 # Heatmap colored with Matplotlib
-fig, ax = plt.subplots(figsize=(10, 8))  # Adjustement of the size (width, height)
+fig, ax = plt.subplots(figsize=(10, 4))  # Adjustement of the size (width, height)
 # cax = ax.imshow(to_plot, cmap='plasma', aspect='auto')  # 'inferno' is a colored pallete
 cax = ax.imshow(to_plot, cmap='viridis', aspect='auto', interpolation='bicubic')
 fig.colorbar(cax, ax=ax)  # color bar
 ax.set_title("Heatmap of the data")  # graphic title
 ax.set_xlabel("X-axis")
 ax.set_ylabel("Y-axis")
+
+
+
+
+
+
 
 # Affichage dans Streamlit
 st.write("Heatmap of the data")
